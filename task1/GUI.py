9ea2b865b1106e2f85dd262810edf53295350d56
maioy97 <mai.algendyfcis@gmail.com>
2019-03-05 18:44:11 +0200

    graphs done , plotting function done

--- task1/GUI.py
+++ task1/GUI.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -14,14 +14,16 @@ class GUI:
     def plot_class(self, class1, class2, features1, features2):
         # for the first part of the task
         # will be called 6 times
-        x, y, lables, class_names = self.read_data(class1, class2, features1, features2)
+        x, y, labels, class_names = self.read_data(GUI, class1, class2, features1, features2)
+        plt.figure(class_names[0]+" vs "+class_names[1])
+        plt.xlabel('X%d' % (features1 + 1))
+        plt.ylabel('X%d' % (features2 + 1))
         x = np.array(x)
         y = np.array(y)
         plt.scatter(x[0:50], y[0:50])
         plt.scatter(x[50::], y[50::])
-        plt.figure(class_names[0]+" vs "+class_names[1])
-        plt.xlable('feature 1')
-        plt.ylable('feature 2')
+
+        plt.show()
 
     def read_data(self, class1, class2, features1, features2):
 
@@ -41,8 +43,8 @@ class GUI:
         class_names[0] = line[4]
         for i in range(start, end):
             line = lines[i].split(',')
-            Xfeatures.append(line[features1])
-            Yfeatures.append(line[features2])
+            Xfeatures.append(float(line[features1]))
+            Yfeatures.append(float(line[features2]))
             labels.append(class1)
 
         start = class2 * 50+1
@@ -51,11 +53,16 @@ class GUI:
         class_names[1] = line[4]
         for i in range(start, end):
             line = lines[i].split(',')
-            Xfeatures.append(line[features1])
-            Yfeatures.append(line[features2])
+            Xfeatures.append(float(line[features1]))
+            Yfeatures.append(float(line[features2]))
             labels.append(class1)
 
         return Xfeatures, Yfeatures, labels, class_names
 
 
-GUI.read_data(GUI, 0, 1, 0, 1)
+GUI.plot_class(GUI, 0, 1, 0, 1)  # x1 x2
+GUI.plot_class(GUI, 0, 1, 0, 2)  # x1 x3
+GUI.plot_class(GUI, 0, 1, 0, 3)  # x1 x4
+GUI.plot_class(GUI, 0, 1, 1, 2)  # x2 x3
+GUI.plot_class(GUI, 0, 1, 1, 3)  # x2 x4
+GUI.plot_class(GUI, 0, 1, 2, 3)  # x3 x4
